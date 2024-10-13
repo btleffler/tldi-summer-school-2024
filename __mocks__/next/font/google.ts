38accb6508f80819b68cd8a95c224fd22fd2a3a1
btleffler <btleffler@gmail.com>
2024-10-13 10:02:56 -0400

    Mock next/font/google

---
+++ __mocks__/next/font/google.ts
@@ -0,0 +1,13 @@
+import { vi } from "vitest";
+
+const mockFont = vi.fn(() => ({
+  className: '.mocked-font-class',
+  style: {
+    fontFamily: 'mockedFontFamily',
+  },
+  variable: '--mocked-font-variable',
+}));
+
+export const Inter = mockFont;
+export const Kalam = mockFont;
+export const Noto_Serif = mockFont;
