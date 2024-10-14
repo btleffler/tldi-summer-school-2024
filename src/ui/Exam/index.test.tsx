6e073dec700cdd55d211c2f72c771572ff8a8ae4
Ben Leffler <btleffler@gmail.com>
2024-10-14 19:35:54 -0400

    Add more mouse tests

--- src/ui/Exam/index.test.tsx
+++ src/ui/Exam/index.test.tsx
@@ -18,6 +18,11 @@ const mockExamData: ExamEntry[] = [
     altText: 'mock altText two',
     answer: 'mock answer two',
     question: 'mock question two',
+  },
+  {
+    altText: 'mock altText three',
+    answer: 'mock answer three',
+    question: 'mock question three',
   }
 ];
 
@@ -35,17 +40,74 @@ describe('UI > Exam', () => {
 
   describe('Keyboard Events', () => {
     it('sets the next entry as active', async () => {
-      render(<Exam data={ mockExamData } />);
-
       const user = userEvent.setup();
 
+      render(<Exam data={ mockExamData } />);
+
+      const entries = screen.getAllByRole('listitem');
+      const list = screen.getByRole('list');
+
+      expect(list.style.pointerEvents).toBe('');
+      expect([...entries[0].classList].includes('active')).toBeTruthy();
+      expect([...entries[1].classList].includes('active')).toBeFalsy();
+      expect([...entries[2].classList].includes('active')).toBeFalsy();
 
       await user.keyboard('{ArrowDown}');
 
-      screen.debug();
+      expect(list.style.pointerEvents).toBe('none');
+      expect([...entries[0].classList].includes('active')).toBeFalsy();
+      expect([...entries[1].classList].includes('active')).toBeTruthy();
+      expect([...entries[2].classList].includes('active')).toBeFalsy();
+
+      await user.keyboard('{ArrowDown}');
+
+      expect([...entries[0].classList].includes('active')).toBeFalsy();
+      expect([...entries[1].classList].includes('active')).toBeFalsy();
+      expect([...entries[2].classList].includes('active')).toBeTruthy();
+
+      await user.keyboard('{ArrowDown}');
+
+      expect([...entries[0].classList].includes('active')).toBeTruthy();
+      expect([...entries[1].classList].includes('active')).toBeFalsy();
+      expect([...entries[2].classList].includes('active')).toBeFalsy();
     });
 
-    it('sets the previous entry as active', () => {});
+    it('sets the previous entry as active', async () => {
+      const user = userEvent.setup();
+
+      render(<Exam data={ mockExamData } />);
+
+      const entries = screen.getAllByRole('listitem');
+      const list = screen.getByRole('list');
+
+      expect(list.style.pointerEvents).toBe('');
+      expect([...entries[0].classList].includes('active')).toBeTruthy();
+      expect([...entries[1].classList].includes('active')).toBeFalsy();
+      expect([...entries[2].classList].includes('active')).toBeFalsy();
+
+      await user.keyboard('{ArrowUp}');
+
+      expect(list.style.pointerEvents).toBe('none');
+      expect([...entries[0].classList].includes('active')).toBeFalsy();
+      expect([...entries[1].classList].includes('active')).toBeFalsy();
+      expect([...entries[2].classList].includes('active')).toBeTruthy();
+
+      await user.keyboard('{ArrowUp}');
+
+      expect([...entries[0].classList].includes('active')).toBeFalsy();
+      expect([...entries[1].classList].includes('active')).toBeTruthy();
+      expect([...entries[2].classList].includes('active')).toBeFalsy();
+
+      await user.keyboard('{ArrowUp}');
+
+      expect([...entries[0].classList].includes('active')).toBeTruthy();
+      expect([...entries[1].classList].includes('active')).toBeFalsy();
+      expect([...entries[2].classList].includes('active')).toBeFalsy();
+    });
+
+    it('does nothing on other input', async () => {
+      render(<Exam data={ mockExamData } />);
+    });
   });
 
   describe('Mouse Events', () => {
