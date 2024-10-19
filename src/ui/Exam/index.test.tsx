72747eab3898186bb5f9f840ef798716ae3ac1c0
btleffler <btleffler@gmail.com>
2024-10-19 09:46:59 -0400

    Finish Exam tests

--- src/ui/Exam/index.test.tsx
+++ src/ui/Exam/index.test.tsx
@@ -61,12 +61,14 @@ describe('UI > Exam', () => {
 
       await user.keyboard('{ArrowDown}');
 
+      expect(list.style.pointerEvents).toBe('none');
       expect([...entries[0].classList].includes('active')).toBeFalsy();
       expect([...entries[1].classList].includes('active')).toBeFalsy();
       expect([...entries[2].classList].includes('active')).toBeTruthy();
 
       await user.keyboard('{ArrowDown}');
 
+      expect(list.style.pointerEvents).toBe('none');
       expect([...entries[0].classList].includes('active')).toBeTruthy();
       expect([...entries[1].classList].includes('active')).toBeFalsy();
       expect([...entries[2].classList].includes('active')).toBeFalsy();
@@ -94,23 +96,69 @@ describe('UI > Exam', () => {
 
       await user.keyboard('{ArrowUp}');
 
+      expect(list.style.pointerEvents).toBe('none');
       expect([...entries[0].classList].includes('active')).toBeFalsy();
       expect([...entries[1].classList].includes('active')).toBeTruthy();
       expect([...entries[2].classList].includes('active')).toBeFalsy();
 
       await user.keyboard('{ArrowUp}');
 
+      expect(list.style.pointerEvents).toBe('none');
       expect([...entries[0].classList].includes('active')).toBeTruthy();
       expect([...entries[1].classList].includes('active')).toBeFalsy();
       expect([...entries[2].classList].includes('active')).toBeFalsy();
     });
 
     it('does nothing on other input', async () => {
+      const user = userEvent.setup();
+
       render(<Exam data={ mockExamData } />);
+
+      const entries = screen.getAllByRole('listitem');
+      const list = screen.getByRole('list');
+
+      await user.keyboard('abcdefghijklmnopqrstuvwxyz');
+
+      expect(list.style.pointerEvents).toBe('');
+      expect([...entries[0].classList].includes('active')).toBeTruthy();
+      expect([...entries[1].classList].includes('active')).toBeFalsy();
+      expect([...entries[2].classList].includes('active')).toBeFalsy();
     });
   });
 
   describe('Mouse Events', () => {
+    it('sets pointer events to auto after setting entry to active via keyboard', async () => {
+      const user = userEvent.setup();
 
+      render(<Exam data={ mockExamData } />);
+
+      const list = screen.getByRole('list');
+
+      await user.keyboard('{ArrowDown}');
+      await user.pointer({
+        coords: {
+          x: 1000,
+          y: 1000,
+        }
+      });
+
+      expect(list.style.pointerEvents).toBe('auto');
+    });
+
+    it('sets an entry to active', async () => {
+      const user = userEvent.setup();
+
+      render(<Exam data={ mockExamData } />);
+
+      const entries = screen.getAllByRole('listitem');
+
+      await user.pointer({
+        target: entries[2],
+      });
+
+      expect([...entries[0].classList].includes('active')).toBeFalsy();
+      expect([...entries[1].classList].includes('active')).toBeFalsy();
+      expect([...entries[2].classList].includes('active')).toBeTruthy();
+    });
   })
 });
